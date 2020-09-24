99c935846b4cd478ed88528f541adb585a9ee941
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-09-24 11:46:47 -0500

    can now count attendees

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,6 +19,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @count = Participation.where("\"#{@event.uuid}\" = true")
   end
 
   def homepage
