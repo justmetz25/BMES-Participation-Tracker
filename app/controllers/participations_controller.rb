088f040118449389c9484e891865c6784fac9c7f
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-09-24 13:12:34 -0500

    require password

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -10,11 +10,15 @@ class ParticipationsController < ApplicationController
     @event_id = params[:event_id]
     @event = Event.find(@event_id)
     @participation.send("#{@event.uuid}=", true)
-    @participation.save
-    render("new")
+    if @event.eventpass == params[:event_pass]
+      @participation.save
+      redirect_to new_participation_path, :flash => { message: "Successfully signed in "}
+    else
+      redirect_to new_participation_path, :flash => { message: "Incorrect password" }
+    end
   end
 
   def participation_params
-    params.require(:participation).permit(:uin, :first_name, :last_name, :email, :event_id)
+    params.require(:participation).permit(:uin, :first_name, :last_name, :email, :event_id, :event_pass)
   end
 end
