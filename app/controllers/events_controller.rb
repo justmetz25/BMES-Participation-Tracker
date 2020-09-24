6ad8b6365eb0ff0f2e768822119466b8cedcdc27
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-09-24 18:10:43 -0500

    move from uuid to event name + event id

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,7 +28,8 @@ class EventsController < ApplicationController
 
   def destroy
     @event = Event.find(params[:id])
-    ActiveRecord::Migration.remove_column :participations, "#{@event.uuid}", :boolean
+    # ActiveRecord::Migration.remove_column :participations, "#{@event.uuid}", :boolean
+    ActiveRecord::Migration.remove_column :participations, "#{@event.title.gsub(" ", "_")}_#{@event.id}", :boolean
     @event.destroy
     flash[:notice] = "Event '#{@event.title}' destroyed successfully."
     redirect_to(events_path)
@@ -47,10 +48,11 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
-    @event.uuid = SecureRandom.uuid.gsub("-", "")
-    ActiveRecord::Migration.add_column :participations, "#{@event.uuid}", :boolean, :null => false, :default => false
+    # @event.uuid = SecureRandom.uuid.gsub("-", "")
+    # ActiveRecord::Migration.add_column :participations, "#{@event.uuid}", :boolean, :null => false, :default => false
 
     if @event.save
+      ActiveRecord::Migration.add_column :participations, "#{@event.title.gsub(" ", "_")}_#{@event.id}", :boolean, :null => false, :default => false
       flash[:notice] = "Event created successfully."
       redirect_to(events_path)
     else
--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -5,16 +5,25 @@ class ParticipationsController < ApplicationController
   end
 
   def create
-    @participation = Participation.new(participation_params)
-    puts "EVENT ID #{params[:event_id]}"
+    @participation = Participation.new
     @event_id = params[:event_id]
-    @event = Event.find(@event_id)
-    @participation.send("#{@event.uuid}=", true)
-    if @event.eventpass == params[:event_pass]
-      @participation.save
-      redirect_to new_participation_path, :flash => { message: "Successfully signed in "}
-    else
-      redirect_to new_participation_path, :flash => { message: "Incorrect password" }
+    @event = Event.find(params[:event_id]) rescue nil
+    puts @event.nil?
+    if @event.nil?
+      redirect_to new_participation_path, :flash => { message: "No such event associated with id" }
+    else 
+      if @event.eventpass == params[:event_pass]
+        # https://stackoverflow.com/questions/52847334/adding-dynamic-attributes-to-a-rails-model ???
+        @participation.instance_eval { class << self; self end }.send(:attr_accessor, "#{@event.title.gsub(" ", "_")}_#{@event.id}")
+        # https://stackoverflow.com/questions/16530532/rails-4-insert-attribute-into-params
+        @participation = Participation.new(participation_params.merge("#{@event.title.gsub(" ", "_")}_#{@event.id}": true))
+        # @participation.send("#{@event.title.gsub(" ", "_")}_#{@event.id}=", true)
+
+        @participation.save
+        redirect_to new_participation_path, :flash => { message: "Successfully signed in" }
+      else
+        redirect_to new_participation_path, :flash => { message: "Incorrect password" }
+      end
     end
   end
 
