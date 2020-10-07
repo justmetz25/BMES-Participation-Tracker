a785bd77ef8df2dbb69d432135b4ecd18382b314
Pablo Ginebra <ginebrapa@tamu.edu>
2020-10-07 17:38:56 -0500

    broken routes

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -7,23 +7,27 @@ class ParticipationsController < ApplicationController
   end
 
   def create
-    @event_id = params[:event_id]
-    @event = Event.find(params[:event_id]) rescue nil
-    puts @event.nil?
-    if @event.nil?
-      redirect_to new_participation_path, :flash => { message: "No such event associated with id" }
-    else 
-      if @event.eventpass == params[:event_pass]
-        @participation = Participation.new(participation_params)
-        @participation.save
-        redirect_to new_participation_path, :flash => { message: "Successfully signed in" }
-      else
-        redirect_to new_participation_path, :flash => { message: "Incorrect password" }
-      end
-    end
+    @participation = Participation.new(participation_params)
+    redirect_to events_path, :flash => { message: "Successfully signed in"}
+    # @event_id = params[:participation]
+    # @event_idd = @event_id.event_idd 
+    # abort @event_idd.inspect
+    # @event = Event.find(@event_id) rescue nil
+    # puts @event.nil?
+    # if @event.nil?
+    #   redirect_to new_participation_path, :flash => { message: @event_id , :event_id => @event_id}
+    # else 
+    #   if @event.eventpass == params[:event_pass]
+    #     @participation = Participation.new(participation_params)
+    #     @participation.save
+    #     redirect_to new_participation_path, :flash => { message: "Successfully signed in" , :event_id => @event_id}
+    #   else
+    #     redirect_to new_participation_path, :flash => { message: "Incorrect password" , :event_id => @event_id}
+    #   end
+    # end
   end
 
   def participation_params
-    params.require(:participation).permit(:uin, :first_name, :last_name, :email, :event_pass).merge(event_id: @event.id)
+    params.require(:participation).permit(:uin, :first_name, :last_name, :email, :event_pass, :event_id)
   end
 end
