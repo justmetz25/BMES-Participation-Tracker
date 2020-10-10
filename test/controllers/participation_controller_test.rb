ac0247002883a81d5a62fbcb41a1d5b6a6ffc381
hwy893747147 <hewangyang@yahoo.com>
2020-10-09 19:19:20 -0500

    clean up

--- test/controllers/participation_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ParticipationControllerTest < ActionDispatch::IntegrationTest
-  test "should get submit" do
-    get participation_submit_url
-    assert_response :success
-  end
-
-end
