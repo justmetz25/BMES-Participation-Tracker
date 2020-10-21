d9cc05b00c9b7a9198eb70e9076735e927b163e8
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-10-21 16:01:54 -0500

    gracefully render nothing if no event exists

--- app/controllers/api/v1_controller.rb
+++ app/controllers/api/v1_controller.rb
@@ -1,21 +1,30 @@
 module Api
-    class V1Controller < ActionController::API
+  class V1Controller < ActionController::API
 
-        def events
-            @events = Event.all
-            render json: @events
-        end
-
-        def event
-            @event = Event.find(params[:id])
-            #render json: @event
-            render json: {
-              id: @event.id,
-              name: @event.title,
-              start_time: @event.starttime,
-              attendees: Participation.where(event_id: @event.id)
-            }
-        end
-        
+    def events
+      @events = Event.all
+      render json: {
+        events: @events
+      }, status: :ok
     end
+
+    def event
+      @event = begin
+                Event.find(params[:id])
+              rescue StandardError
+                nil
+              end
+      if @event.nil?
+        render json: {}, status: :bad_request
+      else
+        render json: {
+          id: @event.id,
+          name: @event.title,
+          start_time: @event.starttime,
+          attendees: Participation.where(event_id: @event.id)
+        }, status: :ok
+      end
+    end
+      
+  end
 end
