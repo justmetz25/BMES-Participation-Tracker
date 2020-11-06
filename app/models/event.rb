ab8640d5d2c845dc65f34bda13f29c609c1fb22f
Alex Ling <22086776+alexling540@users.noreply.github.com>
2020-11-05 21:56:11 -0600

    require certain fields

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,13 @@
 class Event < ApplicationRecord
 
   scope :sorted, -> { order('starttime ASC') }
+  has_many :participation, :dependent => :destroy
+  validates_presence_of :title, :starttime, :eventpass
 
   def as_json(*)
     { id: id, name: title, start_time: starttime }
   end
-has_many :participation, :dependent => :destroy
+  
 #   before_destroy :remove_participation_from_event
 
 # private
